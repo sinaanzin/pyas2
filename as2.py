202fecb270d81c78b6a99697170e71c80fcd9161
sinaanzin <128033761+sinaanzin@users.noreply.github.com>
2025-01-08 15:45:19 +0530

    Add files via upload

---
+++ as2.py
@@ -0,0 +1,33 @@
+rand_num = [4,7,33,7,98]
+print(f"Random numbers are {rand_num}")
+
+rand_num.append(2)
+rand_num.append(0)
+rand_num.append(6)
+print(rand_num)
+
+print("\n")
+
+for n in rand_num:
+    print(n)
+
+info = {'name': 'John',  'age': 25,'address': 'New York'}
+print("Dictionary:",info)
+
+info['phone']=7453627467
+print(info)
+
+set={1,2,3,4,5}
+print(set)
+
+set.add(6)
+print(set)
+
+set.remove(3)
+print(set)
+
+tuple=(1,2,3,4)
+print(tuple)
+
+print("Length of the tuple is",len(tuple))
+
